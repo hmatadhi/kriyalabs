2ee9123200d1d1aecb740167526cec2f1785a9a6
Harsha Matadhikari <harsha.matadhikari@gmail.com>
2017-05-06 18:23:57 +0000

    add site

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+  <head>
+    <title>Kriyalabs - Harsha's Projects</title>
+
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta http-equiv="content-language" content="en" />
+    <meta name="keywords" content="Stocks, Hinduism, Language, Economy, Kannada" />
+    <meta name="description" content="Personal projects of Harsha" />
+    <meta name="author" content="Harsha Matadhikari {harsha.matadhikari@gmail.com}" />
+    <meta name="copyright" content="Copyright (C) 2017 - 2018. kriyalabs.in" />
+    <meta http-equiv="imagetoolbar" content="no" />
+
+    <link type="text/css" rel="stylesheet" media="screen" href="include/css/screen.css" />
+    <link type="text/css" rel="stylesheet" media="print" href="include/css/print.css" />
+    <link type="text/css" rel="stylesheet" media="all" href="include/css/kriya.css" />
+
+    <script type="text/javascript" src="include/js/prototype.js"> </script>
+    <script type="text/javascript" src="include/js/scriptaculous.js"> </script>
+    <script type="text/javascript" src="include/js/kriyalabs.js"> </script>
+</head>
+
+  <body>
+    <header class="top">
+      <img id="headerLogo"
+           src="images/headerLogo.gif" alt="Kriyatrade header logo image">
+      <img id="headerSlogan"
+          src="images/headerSlogan.gif" alt="Helping you with your daily trade">
+    </header>
+
+    <nav>
+    <ul>
+        <li class="selected"><a href="http://kriyalabs.in">HOME</a></li>
+        <li><a href="">BLOG</a></li>
+        <li><a href="">TRIPS</a></li>
+        <li><a href="">MYTHOLOGY</a></li>
+        <li><a href="">FEEDBACK</a></li>
+    </ul>
+    </nav>
+
+    <div id="tableContainer">
+    <div id="tableRow">
+    <section id="projects">
+            <h1>My projects</h1>
+            <list>
+            <li><a href="kriyaTrade/php/index.php"> Kriya Trade  </a></li>
+            <li><a href="labgenie/index.php"> Labgenie  </a></li>
+            <li><a href="dms/index.php"> DocMan  </a></li>
+            </list>
+    </section>
+    <section id="main">
+    <h1>About me</h1>
+    <p>
+    I am Harsha,I work in Cisco.
+    This is my personal site.
+    </p>
+    </section>
+
+    <aside>
+          
+            <h1>My study links</h1>
+            <p>
+            <h2>Scriptaculous</h2>
+            <list>
+            <li><a href="scriptaculous/scriptaculous-js-1.9.0/test/run_unit_tests.html"> scriptaculous unit tests </a> </li>
+            <li><a href="scriptaculous/scriptaculous-js-1.9.0/test/run_functional_tests.html"> scriptaculous functional tests </a> </li>
+            </p>
+
+            <p>
+            <h2>Prototype</h2>
+            <list>
+            <li><a href="prototype/test/browser.html"> prototype tests browser </a></li>
+            <li><a href="prototype/test/console.html"> prototype test console </a></li>
+            </p>
+
+            <p>
+            <h2>HTML CSS</h2>
+            <list>
+            <li><a href="./htmlcss/chapter4/buzz/index.html">4/buzz</a></li>
+            <li><a href="./htmlcss/chapter4/starbuzz/index.html">4/starbuzz</a></li>
+            <li><a href="./htmlcss/chapter12/starbuzz/index.html">12/starbuzz</a></li>
+            <li><a href="./htmlcss/chapter11/tabledisplay/index.html">11/tabledisplay</a></li>
+            <li><a href="./htmlcss/chapter11/starbuzz/index.html">11/starbuzz</a></li>
+            <li><a href="./htmlcss/chapter11/starbuzz-complete/index.html">11/starbuzz-complete</a></li>
+            <li><a href="./htmlcss/chapter11/absolute/index.html">11/absolute</a></li>
+            <li><a href="./htmlcss/chapter14/starbuzz/index.html">14/starbuzz</a></li>
+            <li><a href="./htmlcss/chapter1/starbuzz/index.html">1/starbuzz</a></li>
+            <li><a href="./htmlcss/chapter5/mypod/index.html">5/mypod</a></li>
+            </list>
+            </p>
+    </aside>
+    </div> <!-- tableRow -->
+    </div> <!-- tableContainer -->
+
+    <footer>
+      &copy; 2017, Kriyalabs.in
+      <br>
+      All trademarks and registered trademarks appearing on 
+      this site are the property of their respective owners.
+    </footer>
+
+  </body>
+</html>
+
+
